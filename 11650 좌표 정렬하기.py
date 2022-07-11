15d12cc7415d052012a47f54d2076cf232d9ac53
mingiiiiiiii <95513561+mingiiiiiiii@users.noreply.github.com>
2022-07-11 17:45:47 +0900

    Create 11650 좌표 정렬하기.py

---
+++ 11650 좌표 정렬하기.py
@@ -0,0 +1,9 @@
+import sys
+
+n = int(sys.stdin.readline())
+a = []
+for i in range(n):
+    a.append(list(map(int, sys.stdin.readline().split())))
+a.sort()
+for i in range(n):
+    print(a[i][0], a[i][1])
